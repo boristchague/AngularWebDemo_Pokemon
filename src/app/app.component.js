65d30a2a798710ab6aefe628cd7deab7b372f027
boris <boris.tchague@gmail.com>
2020-09-22 10:52:24 +0200

    Angular_pokemon first commit

---
+++ src/app/app.component.js
@@ -0,0 +1,39 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var mock_pokemons_1 = require("./mock-pokemons");
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = "liste des pokemones";
+        this.value = '';
+        this.age = 20;
+        this.pokemons = null;
+    }
+    AppComponent.prototype.ngOnInit = function () {
+        this.pokemons = mock_pokemons_1.POKEMONS;
+    };
+    AppComponent.prototype.selectPokemon = function (pokemon) {
+        console.log('Vous avez selectionné ' + pokemon.name);
+    };
+    AppComponent.prototype.onClick = function () {
+        console.log("Vous avez cliquez ! ");
+    };
+    AppComponent.prototype.onKey = function (event) {
+        this.value = "Bonjour" + event.target.value;
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'pokemon-app',
+            templateUrl: '../app.component.html'
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
+
+  
+@Component({
+  selector: 'pokemon-app',
+  templateUrl: '../app.component.html'
+})
+export class AppComponent implements OnInit {
+  
+    private title: String = "liste des pokemones";
+    private value: String = '';
+    private age = 20;
+  pokemons: Pokemon[] = null;
+  
+  ngOnInit() {
+
+    this.pokemons = POKEMONS;
+  }
+  
+  selectPokemon(pokemon: Pokemon) {
+    console.log('Vous avez selectionné ' + pokemon.name);
+  }
+    
+  onClick(){
+      console.log("Vous avez cliquez ! ");
+  }
+
+  onKey(event: any){
+      this.value = "Bonjour" + event.target.value;
+  }
+}
